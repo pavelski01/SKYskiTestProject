aa4313fe037abe05fca1d0ea2d59bb99bb739439
paweł.strzeszkowski <pavelski01@gmail.com>
2015-07-15 13:52:11 +0200

    Extract internal class LoginWindow from TestCaseFixture and make it independent outer class

--- skyski-selenium/src/main/java/module/skyski_selenium/fixture/TestCaseFixture.java
+++ skyski-selenium/src/main/java/module/skyski_selenium/fixture/TestCaseFixture.java
@@ -1,19 +1,12 @@
 package module.skyski_selenium.fixture;
 
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.Transferable;
-import java.awt.datatransfer.UnsupportedFlavorException;
-import java.awt.event.KeyEvent;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.junit.runner.Description;
+
+import module.skyski_selenium.config.LoginWindow;
 
 public abstract class TestCaseFixture extends TestActionFixture
 {
@@ -61,60 +54,6 @@ public abstract class TestCaseFixture extends TestActionFixture
 		);
 		super.titleAssertion("SKYski Project", "Load home page");
 	}
-	
-	public class LoginWindow implements Runnable
-	{		
-        @Override
-        public void run()
-        { 
-        	try { login(); }
-        	catch (Exception _e) 
-        	{ TestActionFixture.config.toSystemOut("[WATCHER] " + _e.getMessage()); }
-        }
-
-        public void login() throws AWTException, InterruptedException, UnsupportedFlavorException 
-        {
-        	Thread.sleep(5000);
-        	Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();        	
-        	clipboard.setContents(
-    			new Transferable()
-    			{
-    				public DataFlavor[] getTransferDataFlavors() { return new DataFlavor[0]; }
-
-    				public boolean isDataFlavorSupported(DataFlavor _flavor) { return false; }
-
-    				public Object getTransferData(DataFlavor _flavor) throws UnsupportedFlavorException 
-    				{ throw new UnsupportedFlavorException(_flavor); }
-    			}, 
-    			null
-			);        	
-            Robot robot = new Robot();
-            StringSelection username = 
-        		new StringSelection(
-    				TestActionFixture.config.getStagesData().get(0).getBasicCredentialUser()
-				);
-            clipboard.setContents(username, null);            
-            robot.keyPress(KeyEvent.VK_CONTROL);
-            robot.keyPress(KeyEvent.VK_V);
-            robot.keyRelease(KeyEvent.VK_V);
-            robot.keyRelease(KeyEvent.VK_CONTROL);
-            robot.keyPress(KeyEvent.VK_TAB);
-            robot.keyRelease(KeyEvent.VK_TAB);
-            Thread.sleep(2000);
-            StringSelection passwd = 
-        		new StringSelection(
-    				TestActionFixture.config.getStagesData().get(0).getBasicCredentialPassword()
-				);
-            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(passwd, null);
-            robot.keyPress(KeyEvent.VK_CONTROL);
-            robot.keyPress(KeyEvent.VK_V);
-            robot.keyRelease(KeyEvent.VK_V);
-            robot.keyRelease(KeyEvent.VK_CONTROL);
-            robot.keyPress(KeyEvent.VK_ENTER);
-            robot.keyRelease(KeyEvent.VK_ENTER);
-            Thread.sleep(5000);
-        }
-    }
 	
 	private static boolean setUpIsDone = false;
 }
